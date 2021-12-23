6e7f84a1fba0ab12a98109fbce0eb7b821aef1ed
Richard Chen <richard.chen@databricks.com>
2021-12-23 12:57:08 -0800

    db integration

--- exp/api/v1alpha3/azuremanagedcontrolplane_conversion.go
+++ exp/api/v1alpha3/azuremanagedcontrolplane_conversion.go
@@ -38,6 +38,7 @@ func (src *AzureManagedControlPlane) ConvertTo(dstRaw conversion.Hub) error { //
 	}
 
 	dst.Spec.IdentityRef = restored.Spec.IdentityRef
+	dst.Spec.DisableLocalAccounts = restored.Spec.DisableLocalAccounts
 
 	return nil
 }
